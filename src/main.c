3a62aa1914ff97ac1e734e943cb76c975845c511
Ionut Lupu <lupu.ionut@protonmail.ch>
2017-08-14 20:29:23 +0200

    implement generate html

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <gtk/gtk.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 GtkTextView *g_display_result;
-GtkEntry *g_input_name;
+GtkEntry *g_input_http;
+GtkEntry *g_input_image;
+GtkEntry *g_input_title;
 GtkTextBuffer *display_buffer;
 
 static void destroy(GtkWidget *widget, gpointer data);
+char* concat(const char *s1, const char *s2);
+char* generate_html();
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +26,12 @@ int main(int argc, char *argv[])
     window = GTK_WIDGET(gtk_builder_get_object(builder, "afilink"));
     gtk_builder_connect_signals(builder, NULL);
 
-    g_input_name =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_name"));
+    g_input_http =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_http"));
+    g_input_image =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_image"));
+    g_input_title =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_title"));
     g_display_result =
         GTK_TEXT_VIEW(gtk_builder_get_object(builder, "display_result"));
 
@@ -38,7 +48,9 @@ int main(int argc, char *argv[])
 
 void on_btn_add_clicked()
 {
-    const char *text = gtk_entry_get_text(GTK_ENTRY(g_input_name));
+    char *text = malloc(sizeof(char)*3000);
+    strcpy(text,generate_html());
+
     GtkTextMark *mark;
     GtkTextIter iter;
 
@@ -51,7 +63,10 @@ void on_btn_add_clicked()
         gtk_text_buffer_insert (display_buffer, &iter, "\n", 1);
 
     gtk_text_buffer_insert (display_buffer, &iter, text, -1);
-    gtk_entry_set_text(GTK_ENTRY(g_input_name), "");
+    gtk_entry_set_text(GTK_ENTRY(g_input_http), "");
+    gtk_entry_set_text(GTK_ENTRY(g_input_image), "");
+    gtk_entry_set_text(GTK_ENTRY(g_input_title), "");
+    free(text);
 }
 
 void on_btn_clear_clicked()
@@ -63,4 +78,35 @@ void on_btn_clear_clicked()
 static void destroy(GtkWidget *widget, gpointer data)
 {
     gtk_main_quit();
+}
+
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+char *generate_html()
+{
+    const char *input_http = NULL;
+    const char *input_image = NULL;
+    const char *input_title = NULL;
+    const char *text = NULL;
+
+    input_http = gtk_entry_get_text(GTK_ENTRY(g_input_http));
+    input_image = gtk_entry_get_text(GTK_ENTRY(g_input_image));
+    input_title = gtk_entry_get_text(GTK_ENTRY(g_input_title));
+    char *html = malloc(sizeof(char)*3000);
+    text =
+"<figure class='wp-caption alignnone'><a href='%s' target='_blank'>\
+<img class='wp-image-1180' src='%s' alt=''></a>\
+<label class='prod-title'>%s</label><span class='btn-cumpara'>\
+<a href='%s' target='_blank'>\
+<img class='cumpara1 size-full wp-image-1071'\
+src='%s' alt='cumpara' width='142' height='35'></a></span></figure>"; 
+
+    sprintf(html,text,input_http,input_image,input_title,input_http,input_image);
+    return html;
 }
