f49d7ec69f8f1c0857823272694621847cdbe55d
Ionut Lupu <lupu.ionut@protonmail.ch>
2017-08-14 16:49:47 +0200

    implement affiliate link and result box

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <gtk/gtk.h>
 
-GtkWidget *g_lbl_hello;
-GtkWidget *g_lbl_count;
+
+GtkTextView *g_display_result;
+GtkEntry *g_input_name;
+GtkTextBuffer *display_buffer;
+
+static void destroy(GtkWidget *widget, gpointer data);
 
 int main(int argc, char *argv[])
 {
-    GtkBuilder      *builder; 
+    GtkBuilder      *builder;
     GtkWidget       *window;
 
     gtk_init(&argc, &argv);
@@ -13,33 +17,50 @@ int main(int argc, char *argv[])
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "afilink"));
     gtk_builder_connect_signals(builder, NULL);
-    
-    g_lbl_hello = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_hello"));
-    g_lbl_count = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_count"));
+
+    g_input_name =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_name"));
+    g_display_result =
+        GTK_TEXT_VIEW(gtk_builder_get_object(builder, "display_result"));
+
+
+    g_signal_connect (window, "destroy",
+                  G_CALLBACK (destroy), NULL);
 
     g_object_unref(builder);
-
-    gtk_widget_show(window);                
+    gtk_widget_show(window);
     gtk_main();
 
     return 0;
 }
 
-void on_btn_hello_clicked()
+void on_btn_add_clicked()
 {
-    static unsigned int count = 0;
-    char str_count[30] = {0};
-    
-    gtk_label_set_text(GTK_LABEL(g_lbl_hello), "Hello, world!");
-    count++;
-    sprintf(str_count, "%d", count);
-    gtk_label_set_text(GTK_LABEL(g_lbl_count), str_count);
+    const char *text = gtk_entry_get_text(GTK_ENTRY(g_input_name));
+    GtkTextMark *mark;
+    GtkTextIter iter;
+
+    display_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(g_display_result));
+
+    mark = gtk_text_buffer_get_insert (display_buffer);
+    gtk_text_buffer_get_iter_at_mark (display_buffer, &iter, mark);
+
+    if (gtk_text_buffer_get_char_count(display_buffer))
+        gtk_text_buffer_insert (display_buffer, &iter, "\n", 1);
+
+    gtk_text_buffer_insert (display_buffer, &iter, text, -1);
+    gtk_entry_set_text(GTK_ENTRY(g_input_name), "");
 }
 
-// called when window is closed
-void on_window_main_destroy()
+void on_btn_clear_clicked()
+{
+    display_buffer = NULL;
+    gtk_text_view_set_buffer(GTK_TEXT_VIEW(g_display_result), display_buffer);
+}
+
+static void destroy(GtkWidget *widget, gpointer data)
 {
     gtk_main_quit();
 }
